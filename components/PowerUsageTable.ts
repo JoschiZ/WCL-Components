389d59e34f8a060d84b97538c3b4c347d63bd440
Joschi <joschi.grey@posteo.de>
2023-10-04 19:46:27 +0200

    Added multi fight aggregation support

--- components/PowerUsageTable.ts
+++ components/PowerUsageTable.ts
@@ -3,6 +3,7 @@ import {RpgLogs} from "../definitions/RpgLogs";
 import {eventsByCategoryAndDisposition} from "../util/wrappers/getEventsByTypeAndDisposition";
 import GetResourceName from "../util/GetResourceName";
 import CastEvent = RpgLogs.CastEvent;
+import getAbilityMarkdown from "../util/getAbilityMarkdown";
 
 const COMPONENT_NAME = "Resource Usage"
 const DEBUG = false
@@ -17,15 +18,26 @@ type Row = {
 
 export default getComponent = () => {
     const allFightData: FightData[] = []
+    const allRows: Row[][] = []
+    const allColumns: Record<string, RpgLogs.TableColumn>[] = []
     for (const fight of reportGroup.fights) {
-        allFightData.push(ParseFight(fight))
+        const fightData = ParseFight(fight)
+        const column = fightData.GetColumns()
+        allFightData.push(fightData)
+        allColumns.push(column)
+        allRows.push(fightData.GetRows(column))
     }
     LOGGER.addMessage("Fight Data", allFightData)
 
-    const columns = allFightData[0].GetColumns()
+    const columns = mergeColumns(allColumns)
     LOGGER.addMessage("Columns", columns)
 
-    const data = allFightData[0].GetRows(columns)
+    let data = mergeAllRows(allRows)
+    if (allFightData.length > 1){
+        // we need to recalculate all the percentages
+        data = updatePercentages(data, allFightData)
+    }
+
     LOGGER.addMessage("Data Rows", data)
 
 
@@ -35,7 +47,7 @@ export default getComponent = () => {
             columns: {
                 title: {
                     header: COMPONENT_NAME,
-                    columns: allFightData[0].GetColumns()
+                    columns: columns
                 },
             },
             data: data
@@ -47,6 +59,43 @@ export default getComponent = () => {
     }
 
     return returnTable
+}
+
+function updatePercentages(allRows: Row[], allFightData: FightData[]){
+    const completeResourceMap: Record<string, Record<string, number>> = {}
+
+    // build a complete map for all fights and players
+    for (const fightData of allFightData){
+        for (const player in fightData.players){
+            const resourcesUsed = fightData.players[player].getTotalResourcesUsed()
+            if (!completeResourceMap[player]){
+                completeResourceMap[player] = resourcesUsed
+            } else {
+                for (const resourceName in resourcesUsed) {
+                    completeResourceMap[player][resourceName] ??= 0
+                    completeResourceMap[player][resourceName] += resourcesUsed[resourceName]
+                }
+            }
+        }
+    }
+
+    for (const row of allRows){
+        const totalResourceUse = completeResourceMap[row.actorName]
+
+        for (const columnHeader in row){
+            if (!columnHeader.endsWith("%")){
+                continue
+            }
+            const resourceName = columnHeader.slice(0, -1);
+            const usageOfResourceInRow = row[resourceName]
+            if (typeof usageOfResourceInRow === "number" && usageOfResourceInRow !== 0){
+                const percentage = usageOfResourceInRow / totalResourceUse[resourceName]
+                row[columnHeader] = Math.round(percentage * 100)
+            }
+        }
+    }
+
+    return allRows
 }
 
 function ParseFight(fight: RpgLogs.Fight): FightData {
@@ -63,10 +112,90 @@ function ParseFight(fight: RpgLogs.Fight): FightData {
     return fightData
 }
 
+function mergeAllRows(rowLists: Row[][]): Row[] {
+    // Create a map to hold rows by actorName and ability for faster lookup
+    const map = new Map<string, Row>();
+
+    // Process each row of each list
+    rowLists.forEach(rows => {
+        rows.forEach((row) => {
+            const key = generateKey(row);
+            mergeRowToMap(map, key, row);
+        });
+    });
+
+    // Return the merged list
+    return Array.from(map.values());
+}
+
+// A function to generate a unique key for each row based on actorName and ability
+function generateKey(row1: Row): string {
+    return `${row1.actorName}_${row1.ability}`;
+}
+
+// A function to merge a row into the existing map
+// If a row with the same key exists, the new row will be merged into it
+function mergeRowToMap(map: Map<string, Row>, key: string, row: Row) {
+    let resultRow = map.get(key);
+
+    if (resultRow) {
+        mergeRows(resultRow, row);
+    } else {
+        resultRow = { ...row };
+    }
+
+    map.set(key, resultRow);
+}
+
+function mergeRows(row1: Row, row2: Row): Row {
+    // Check if rows have the same actorName and ability
+    if (row1.actorName === row2.actorName && row1.ability === row2.ability) {
+        for (const prop in row1) {
+            let value1 = row1[prop];
+            let value2 = row2[prop];
+
+            // If one value is string and the other is number, try converting string to float
+            if (typeof value1 === 'string' && typeof value2 === 'number') {
+                value1 = parseFloat(value1);
+            } else if (typeof value2 === 'string' && typeof value1 === 'number') {
+                value2 = parseFloat(value2);
+            }
+
+            // If both values are numbers, sum them
+            if (typeof value1 === 'number' && typeof value2 === 'number') {
+                row1[prop] = value1 + value2;
+            }
+            // If both values are strings, try to parse them
+            else if (typeof value1 === 'string' && typeof value2 === 'string') {
+                const parsed1 = parseFloat(value1);
+                const parsed2 = parseFloat(value2);
+                if (!isNaN(parsed1) && !isNaN(parsed2)) {
+                    row1[prop] = parsed1 + parsed2;
+                }
+            }
+        }
+
+        return row1;
+    } else {
+        throw new Error('Row objects must have the same actorName and ability');
+    }
+}
+
+function mergeColumns(columnsList: Array<Record<string, RpgLogs.TableColumn>>): Record<string, RpgLogs.TableColumn> {
+    let mergedColumns: Record<string, RpgLogs.TableColumn> = {};
+
+    for (let columns of columnsList) {
+        mergedColumns = {...mergedColumns, ...columns};
+    }
+
+    return mergedColumns;
+}
+
 
 
 class FightData {
     public readonly players: Record<string, ActorData> = {}
+    public readonly AbilitiesUsed: Record<string, RpgLogs.Ability> = {}
 
     public AddCastEvent(event: RpgLogs.CastEvent){
         if (!event.source){
@@ -75,6 +204,10 @@ class FightData {
         if (event.source.type === "Pet"){
             return;
         }
+        if (!event.ability){
+            return;
+        }
+        this.AbilitiesUsed[event.ability.name] = event.ability
         this.players[event.source.name] ??= new ActorData()
         this.players[event.source.name].AddCastEvent(event)
     }
@@ -128,18 +261,18 @@ class FightData {
                 const abilityData = this.players[playerName].abilityDataByName[abilityName]
                 const row: Row = {
                     actorName: playerName,
-                    ability: abilityName,
+                    ability: getAbilityMarkdown(this.AbilitiesUsed[abilityName]),
                     castCount: abilityData.Casts
                 }
                 for (const resourceName in abilityData.totalResourcesUsed) {
-                    const resourceUsedDisplay = abilityData.totalResourcesUsed[resourceName] ? abilityData.totalResourcesUsed[resourceName] : "-"
-                    const resourcePercentDisplay = totalUsed[resourceName] ? Math.round((abilityData.totalResourcesUsed[resourceName] / totalUsed[resourceName])*100) : "-"
+                    const resourceUsedDisplay = abilityData.totalResourcesUsed[resourceName] ? abilityData.totalResourcesUsed[resourceName] : 0
+                    const resourcePercentDisplay = totalUsed[resourceName] ? Math.round((abilityData.totalResourcesUsed[resourceName] / totalUsed[resourceName])*100) : 0
                     row[resourceName] = resourceUsedDisplay
                     row[resourceName+"%"] = resourcePercentDisplay
                 }
 
                 for (const columnKey in columns){
-                    row[columnKey] ??= "-"
+                    row[columnKey] ??= 0
                 }
 
                 dataRows.push(row)
--- util/GetResourceName.ts
+++ util/GetResourceName.ts
@@ -1,59 +1,33 @@
-export default function GetResourceName (resourceType: number): string{
 
-    switch (resourceType) {
-        case 0:
-            return "Mana"
-        case 1:
-            return "Rage"
-        case 2:
-            return "Focus"
-        case 3:
-            return "Energy"
-        case 4:
-            return "Combo Points"
-        case 5:
-            return "Runes"
-        case 6:
-            return "Runic Power"
-        case 7:
-            return "Soul Shards"
-        case 8:
-            return "Astral Power"
-        case 9:
-            return "Holy Power"
-        case 10:
-            return "Alternate"
-        case 11:
-            return "Maelstrom"
-        case 12:
-            return "Chi"
-        case 13:
-            return "Insanity"
-        case 14:
-            return "Obsolete"
-        case 15:
-            return "Obsolete2"
-        case 16:
-            return "Arcane Charges"
-        case 17:
-            return "Fury"
-        case 18:
-            return "Pain"
-        case 19:
-            return "Essence"
-        case 20:
-            return "Rune Blood (Classic)"
-        case 21:
-            return "Rune Frost (Classic)"
-        case 22:
-            return "Rune Unholy (Classic)"
-        case 23:
-            return "Alternate Quest"
-        case 24:
-            return "Alternate Encounter"
-        case 25:
-            return "Alternate Mount"
-        default:
-            return resourceType + ""
-    }
+const resources: {[key: number]: string} = {
+    0: "Mana",
+    1: "Rage",
+    2: "Focus",
+    3: "Energy",
+    4: "Combo Points",
+    5: "Runes",
+    6: "Runic Power",
+    7: "Soul Shards",
+    8: "Astral Power",
+    9: "Holy Power",
+    10: "Alternate",
+    11: "Maelstrom",
+    12: "Chi",
+    13: "Insanity",
+    14: "Obsolete",
+    15: "Obsolete2",
+    16: "Arcane Charges",
+    17: "Fury",
+    18: "Pain",
+    19: "Essence",
+    20: "Rune Blood (Classic)",
+    21: "Rune Frost (Classic)",
+    22: "Rune Unholy (Classic)",
+    23: "Alternate Quest",
+    24: "Alternate Encounter",
+    25: "Alternate Mount"
+}
+
+export default function GetResourceName(resourceType: number): string {
+    return resources[resourceType] || `${resourceType}`;
 }
